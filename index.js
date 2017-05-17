2cd516d7492d9ecbc816609c6b20186e3cdc9ae9
T-Dex <dex.m.mills@gmail.com>
2017-05-16 19:40:00 -0600

    Final Notes

--- index.js
+++ index.js
@@ -7,17 +7,16 @@
 // var string="This is a string",
 // var boolean = true // or false statement
 // var number= 1 // 2,3,4,5...,
-// var obj= {data: "structure"},
-// var array= [ /* Index collection of Data */ ]
-//
+// // var obj= {data: "structure"},
+// // var array= [ /* Index collection of Data */ ]
+// //
 // function bar(){//this is the start of a local or block scope
 //     var foo;
-//     //var foo's data will be taken outside of the scope and moved to the global scope
 //     foo = 3;
 //     console.log(foo);
 // }
 // bar();
-//
+// //
 // function foo(){
 //     bar = 4;
 //     // the computer reads the var first and moves it to the top right after the curly braces
@@ -45,72 +44,72 @@
 // }
 //
 // ourSecondVarFunc();
+
 //
-//
-// //Temporal Dead Zone
-//
-// // function yee(){
-// //     haw = 5;
-// //     //temporal dead zone nothing is hoisted to the top of the block
-// //     let haw = 4;
-// //   console.log(haw);
-// // }
-// //  yee();
-//
-//  //let
-//  //introduced in ES6
-//  //Mirrors many of the items of var when coming to redeclaring.
-//  // Big change was where the data gets pushed to. let hijacks the block and pushes the data to the top of the block only not all the way to the global.
-//  function barToo(){//this is the start of a local or block scope
-//      let foo;
-//      //let foo's data will be taken outside of the scope and moved to the global scope.
-//      foo = 666;
-//      console.log(foo);
-//  }
-//  barToo();
-//
-//  //CONST
-//  //This was the second keyword that was intorduced in ES6.
-//
-//  const whoop = 5;
-//  // Redeclaring a const will throw an error
-//  whoop = 5;
-//
-// // Both let and const help control the mutabilty and scope of the state. var has tradionally been the only way to pass variable data and with ES6 there is new options to pass the variable data along.
-// // Closures
-//
-//
-// debugger;
-//
-// // var in this scenario is run over each time and returns a new number. The scope is closed each iteration and rerun. Var will be logged as "6". Var in this instance is outside the function and runs until the 'for' requirements are fulfilled.
-// for (var i=1; i<=5; i++) {
-// 	(function(){
-// 		setTimeout( function timer(){
-// 			console.log( i );
-// 		}, i*1000 );
-// 	})();
+//Temporal Dead Zone
+
+// function yee(){
+//     haw = 5;
+//     //temporal dead zone nothing is hoisted to the top of the block
+//     let haw = 4;
+//   console.log(haw);
 // }
-// // let hijacks the block and returns "i" each pass thru. It does not run the full loop each time and return the same number. This Syntax is a lot cleaner and returns i for each loop thru. Var runs thru all iterations of the loop before returning the data.
-// for (let i=1; i<=5; i++) {
-// 	setTimeout( function timer(){
-// 		console.log( i );
-// 	}, i*1000 );
-// }
-//
-// //The examples above do log different numbers because of how "var" and "let" let the function work with the data "i" and the use of them each time it is run thru the function.
-// //This also shows closure as well. The Data is built and retained with in the block of the function and garbage collect once the block is finshed.
-// 
-//
-//
-// //Closures are when a var/let/const is contained within a block but can be used. This contains the data that is needed with in the block but does not allow it to be accessed out side the block.
-//
-// function closureFunc() {
-//   const name = 'Zoidberg';// declaring the const data in the inital block
-//   function displayName() {//new block
-//     console.log(name);//const is displayed
-//   }
-//   return displayName;
-// }
-//
-// const myFunc = closureFunc();
-// myFunc();
+//  yee();
+
+ //let
+ //introduced in ES6
+ //Mirrors many of the items of var when coming to redeclaring.
+ // Big change was where the data gets pushed to. let hijacks the block and pushes the data to the top of the block only not all the way to the global.
+ // function barToo(){//this is the start of a local or block scope
+ //     let foo = 1;
+ //     //let foo's data will be taken outside of the scope and moved to the global scope.
+ //     foo = 1234;
+ //     console.log(foo);
+ // }
+ // barToo();
+ //
+ // //CONST
+ // //This was the second keyword that was intorduced in ES6.
+ //
+ // const whoop = 5;
+ // // Redeclaring a const will throw an error
+ // whoop = 5;
+
+// Both let and const help control the mutabilty and scope of the state. var has tradionally been the only way to pass variable data and with ES6 there is new options to pass the variable data along.
+// Closures
+
+
+debugger;
+
+// var in this scenario is run over each time and returns a new number. The scope is closed each iteration and rerun. Var will be logged as "6". Var in this instance is outside the function and runs until the 'for' requirements are fulfilled.
+for (var i=1; i<=4; i++) {
+	(function(){
+		setTimeout( function timer(){
+			console.log( i );
+		}, i*1000 );
+	})();
+}
+// let hijacks the block and returns "i" each pass thru. It does not run the full loop each time and return the same number. This Syntax is a lot cleaner and returns i for each loop thru. Var runs thru all iterations of the loop before returning the data.
+for (let i=1; i<=6; i++) {
+	setTimeout( function timer(){
+		console.log( i );
+	}, i*1000 );
+}
+
+//The examples above do log different numbers because of how "var" and "let" let the function work with the data "i" and the use of them each time it is run thru the function.
+//This also shows closure as well. The Data is built and retained with in the block of the function and garbage collect once the block is finshed.
+
+
+
+//Closures are when a var/let/const is contained within a block but can be used. This contains the data that is needed with in the block but does not allow it to be accessed out side the block.
+
+function closureFunc() {
+  let name = 'Zoidberg';// declaring the const data in the inital block
+  function displayName() {//new block
+    console.log(name);//const is displayed
+  }
+  return displayName;
+}
+console.log(name);
+const myFunc = closureFunc();
+myFunc();
